52cb81a4a865e22caeeb9b0b4e926bfe4953f04b
Adam Sabet <arishel@gmail.com>
2023-05-22 21:42:18 -0700

    first commit

---
+++ TestMain.java
@@ -0,0 +1,33 @@
+
+/*
+* Programmer: Adam Sabet
+* Class: CS145 with Jaramiah Ramsey
+* Date Due: 5/23/2023
+* Assignment 1: Word Search Generator
+
+* This program will do the following: 
+    * 
+* For extra credit I: 
+    * 
+*/
+
+import java.util.Scanner; // program uses the Scanner for the library
+
+
+public class TestMain {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        printIntro();
+        WordSearchManager wordSearch = new WordSearchManager();
+
+    }
+
+     
+ /*------------------------------------------------------------------------------------------- */
+/*This method prints out the intro to the program. It gives the user an idea of what the program 
+does and how to use it. */
+ public static void printIntro() {
+    
+ }
+}
---
+++ WordSearchManager.java
@@ -0,0 +1,32 @@
+import java.lang.reflect.Array;
+
+public class WordSearchManager {
+    private char[][] wordSearch;
+    
+    // Default constructor
+    public WordSearchManager(int size) {
+        this.wordSearch = new char[size][size];
+    }
+
+    /*---------------------------------------------------------------------------------------- */
+    /*This method does the bulk of the work. It will prompt the user for how many words and what 
+    the words are and generate a word search based on those words. 
+    */    
+    public static void generate() {
+        
+    }
+ 
+ 
+
+    /*---------------------------------------------------------------------------------------- */
+    /*This method prints the current word search that has been generated. */
+    public static void print () {
+    
+    }
+
+    /*---------------------------------------------------------------------------------------- */
+    /*This method prints the solution to the word search that has been generated. */
+    public static void showSolution () {
+    
+    }
+}
